d1565b43f91ce914603f4bbf11fad4ec973f7aa5
lurak <lurak@ukr.net>
2019-10-16 19:03:41 +0300

    Add: my_str_empty

--- include/my_str_t.h
+++ include/my_str_t.h
@@ -13,11 +13,9 @@ typedef struct
 int my_str_create(my_str_t* str, size_t buf_size);
 int my_str_from_cstr(my_str_t* str, const char* cstr, size_t buf_size);
 void my_str_free(my_str_t* str);
-<<<<<<< HEAD
 size_t my_str_capacity(const my_str_t* str);
-=======
 int my_str_getc(const my_str_t* str, size_t index);
->>>>>>> 99258ebaa54942b72ff05eee61d0277d9043e51f
+int my_str_empty(const my_str_t* str);
 
 #endif
 
