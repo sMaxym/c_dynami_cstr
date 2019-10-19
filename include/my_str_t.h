8e772b09fc5a7c969e5a8f69f2cc9596f14322c5
smaxym <02smaxym@gmail.com>
2019-10-19 17:55:16 +0300

    add: my_str_write

--- include/my_str_t.h
+++ include/my_str_t.h
@@ -50,6 +50,7 @@ size_t my_str_find_if(const my_str_t* str, int (*predicat)(int));
 int my_str_read_file(my_str_t* str, FILE* file);
 int my_str_read(my_str_t* str);
 int my_str_write_file(const my_str_t* str, FILE* file);
+int my_str_write(const my_str_t* str);
 
 #endif
 
